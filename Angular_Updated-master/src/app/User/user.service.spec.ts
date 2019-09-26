93fb53e55adc6bf87379875bd50390a2c7f1d1a7
LavishVerma <47637741+LavishVerma@users.noreply.github.com>
2019-09-26 11:50:57 +0530

    Update user.service.spec.ts

--- Angular_Updated-master/src/app/User/user.service.spec.ts
+++ Angular_Updated-master/src/app/User/user.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -7,7 +7,7 @@ describe('UserService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       
-      HttpClientModule
+      HttpClientTestingModule
         ]
   }));
 
